263336ee60fb8e643672ebc4853638bc0c257353
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-07 23:31:03 -0300

    Add project files.

---
+++ CompararStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿string frase, fraseCompara;
+
+Console.Write("Digite alguns caracteres: ");
+frase = Console.ReadLine();
+
+Console.Write("Digite mais alguns caracteres: ");
+fraseCompara = Console.ReadLine();
+
+if (frase.Equals(fraseCompara))
+    Console.Write("\nAs strings são iguais\n");
+else
+    Console.Write("\nAs strings são diferentes\n");
